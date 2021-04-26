b2b255930f0c624158c605e6763149623ee42373
Christopher Bunn <github@thenerdery.org>
2021-04-26 16:37:20 +0900

    Reformat some long lines.

--- elitebusiness/make_presentation.py
+++ elitebusiness/make_presentation.py
@@ -18,10 +18,12 @@ def underline_vocab(target: str, vocabs: list[str]) -> str:
 # Get data from google Sheet (one level at a time)
 def get_data_for_level(level: str) -> list[str]:
     # Fetch data from Google Sheet
-    scope = ["https://spreadsheets.google.com/feeds",
-             "https://www.googleapis.com/auth/spreadsheets",
-             "https://www.googleapis.com/auth/drive.file",
-             "https://www.googleapis.com/auth/drive"]
+    scope = [
+        "https://spreadsheets.google.com/feeds",
+        "https://www.googleapis.com/auth/spreadsheets",
+        "https://www.googleapis.com/auth/drive.file",
+        "https://www.googleapis.com/auth/drive",
+    ]
     creds = ServiceAccountCredentials.from_json_keyfile_name("creds.json", scope)
     client = gspread.authorize(creds)
     sheet = client.open("Copy of ビジネス英語研修_online_対訳付き").worksheet(f"level_{level}")
@@ -29,7 +31,9 @@ def get_data_for_level(level: str) -> list[str]:
 
 
 # Create template mapping for one lesson at a time
-def create_template_mapping(data: list, level: int, unit: int, lesson: int) -> dict[str, str]:
+def create_template_mapping(
+    data: list, level: int, unit: int, lesson: int
+) -> dict[str, str]:
     # Set the row based on the unit and lesson, column to 1
     # *NOTE* Google Sheets and gspread start numbering of rows
     #  and columns at 1, while the Python dict begins at 0,0
@@ -44,7 +48,7 @@ def create_template_mapping(data: list, level: int, unit: int, lesson: int) -> d
     # These are used for the page header
     template_mapping["unit"] = unit
     template_mapping["lesson"] = lesson
-    print(f'Level: {level}, Unit: {unit}, Lesson: {lesson}')
+    print(f"Level: {level}, Unit: {unit}, Lesson: {lesson}")
     template_mapping["unit_title"] = data[unit_row][column]
     template_mapping["lesson_title"] = data[row][column + 1]
     template_mapping["dialogue_a1_en"] = data[row][column + 4]
@@ -61,8 +65,12 @@ def create_template_mapping(data: list, level: int, unit: int, lesson: int) -> d
     vocab2_en = data[row + 1][column + 8]
     vocab3_en = data[row + 2][column + 8]
     vocab4_en = data[row + 3][column + 8]
-    template_mapping["target_a_en"] = underline_vocab(target_a_en, [vocab1_en, vocab2_en, vocab3_en, vocab4_en])
-    template_mapping["target_b_en"] = underline_vocab(target_b_en, [vocab1_en, vocab2_en, vocab3_en, vocab4_en])
+    template_mapping["target_a_en"] = underline_vocab(
+        target_a_en, [vocab1_en, vocab2_en, vocab3_en, vocab4_en]
+    )
+    template_mapping["target_b_en"] = underline_vocab(
+        target_b_en, [vocab1_en, vocab2_en, vocab3_en, vocab4_en]
+    )
     template_mapping["vocab1_en"] = data[row][column + 8]
     template_mapping["vocab2_en"] = data[row + 1][column + 8]
     template_mapping["vocab3_en"] = data[row + 2][column + 8]
@@ -96,34 +104,42 @@ def fill_template(template: str, template_mapping: dict[str, str]) -> str:
 # Output PDF
 def output_pdf(contents: str, filename: str):
     # Log WeasyPrint output
-    logger = logging.getLogger('weasyprint')
-    logger.addHandler(logging.FileHandler('/tmp/weasyprint.log'))
+    logger = logging.getLogger("weasyprint")
+    logger.addHandler(logging.FileHandler("/tmp/weasyprint.log"))
     # Create Weasyprint HTML object
     html = HTML(string=contents)
     # Output PDF via Weasyprint
     html.write_pdf(filename)
 
 
-def main(levels: list,
-         units: list,
-         lessons: list,
-         template_filename: str = 'EB-presentation-template.html',
-         output_path: str = '/mnt/c/Users/chris/projects/elitebusiness/output/'):
+def main(
+    levels: list,
+    units: list,
+    lessons: list,
+    template_filename: str = "EB-presentation-template.html",
+    output_path: str = "/mnt/c/Users/chris/projects/elitebusiness/output/",
+):
     for level in levels:
-        print(f'Level {level}')
+        print(f"Level {level}")
         # Get data from Google Sheet
         data = get_data_for_level(level)
         # Loop through all Units and Lessons
         for unit in units:
             for lesson in lessons:
                 # create mapping dict
-                template_mapping = create_template_mapping(data=data, level=level, unit=unit, lesson=lesson)
+                template_mapping = create_template_mapping(
+                    data=data, level=level, unit=unit, lesson=lesson
+                )
                 # Get contents of HTML template file
                 template_file_contents = get_template(filename=template_filename)
                 # Substitute
-                template_filled = fill_template(template=template_file_contents, template_mapping=template_mapping)
+                template_filled = fill_template(
+                    template=template_file_contents, template_mapping=template_mapping
+                )
                 # Output PDF
-                output_filename = f'{output_path}/Level {level}/EB{level}U{unit}L{lesson}.pdf'
+                output_filename = (
+                    f"{output_path}/Level {level}/EB{level}U{unit}L{lesson}.pdf"
+                )
                 output_pdf(contents=template_filled, filename=output_filename)
 
 
